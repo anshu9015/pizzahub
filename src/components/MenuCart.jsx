6c7fd6e1f6a5ece2373e31b84efe2a87c03d2c15
anshu9015 <anshu.kmr9015@gmail.com>
2023-10-25 01:02:35 +0530

    card css apply

--- src/components/MenuCart.jsx
+++ src/components/MenuCart.jsx
@@ -1,40 +1,47 @@
 import React from 'react'
 
-const MenuCart = ({myData}) => {
-    
+const MenuCart = ({ myData }) => {
 
 
-  return (
-    <>
-        
-        <section className="grid grid-cols-3 gap-16 justify-center">
-        {myData.map((currEle) =>{   // i used this map for DRY (Don't Reapet Yourself)
-            //Destructuring. we use DRY. Now we have not neet to write currEle.id in line n0 16 and no need to write currEle.name etc
-            const {id, name, category, image, description} = currEle;  
-            return (
-                <>
-                    <div className="mt-5 grid grid-cols-1 justify-center" key={id}>
-                <div className="bg-white p-7 relative shadow-md z-10">
-                  <div className="inline-block">
-                    <span className="card-number card-circle subtle">{id}</span>
-                    <span className="card-author subtle">{name}</span>
-                    <h2 className="card-title">{name}</h2>
-                    <span className="card-description subtle">
-                    {description}
-                    </span>
-                    <div className="card-read">Read</div>
-                  </div>
-                  <img src={image} alt="images" className="card-media" />
 
-                  <span className="card-tag  subtle">Order Now</span>
-                </div>
-              </div> 
-                </>
-            ) 
-        })}
-        </section>
-    </>
-  )
+    return (
+        <>
+
+            <section className="grid grid-cols-3 gap-16 justify-center">
+                {myData.map((currEle) => {   // i used this map for DRY (Don't Reapet Yourself)
+                    //Destructuring. we use DRY. Now we have not neet to write currEle.id in line n0 16 and no need to write currEle.name etc
+                    const { id, name, type, rating, image, description, price } = currEle;
+                    return (
+                        <>
+                            <div className="mt-5 grid grid-cols-1 justify-center" key={id}>
+                                <div className="bg-white p-7 relative shadow-md z-10">
+                                    <div className="inline-block">
+                                        <span className="inline-block mt-4 border border-gray-300 rounded-full inline-block h-6 w-6 text-center leading-6 text-xs subtle">{id}</span>
+                                        {/* <span className="block text-xs uppercase tracking-wider mt-4 subtle">{rating}</span> */}
+                                        <h2 className="font-serif text-5xl font-light leading-60 mt-2 capitalize">{name}</h2>
+                                        <span className="font-light leading-6 mt-2 subtle">
+                                            {description}
+                                        </span>
+                                        <div className="before:cursor-pointer before:text-xs before:font-semibold before:tracking-wider before:mt-1 before:mb-5 before:relative before:text-right before:uppercase after:block after:bg-gray-300 after:h-0.5 after:w-3/4 after:absolute after:top-3">Read</div>
+                                    </div>
+                                    <img src={image} alt="images" className="max-w-full h-auto" />
+                                    <div className=''>
+                                    <div className='flex justify-start gap-5'>
+                                        <span className="block text-xs uppercase tracking-wider mt-4 subtle">{rating}</span>
+                                        <span className="block text-xs uppercase tracking-wider mt-4 subtle">{price}</span> 
+                                    </div>
+                                    <div className='flex justify-end'>
+                                       <span className="float-right mt-1 border border-gray-300 py-1 px-2 cursor-pointer hover:bg-gray-300 hover:text-black subtle">Add now</span>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })}
+            </section>
+        </>
+    )
 }
 
 export default MenuCart
